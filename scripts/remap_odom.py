aec2a9ddc141bdbd2155ca8a8c78ecec12bf60c7
VaibhavSanjay <vsanjay@terpmail.umd.edu>
2022-12-08 10:20:48 -0800

    publish remapped odometry

--- scripts/remap_odom.py
+++ scripts/remap_odom.py
@@ -3,10 +3,19 @@ import rospy
 
 from nav_msgs.msg import Odometry
 
-def odometryCb(msg):
-    print msg.pose.pose
+pub = rospy.Publisher("/mavros/odometry/out", Odometry, queue_size=10)
+
+def callback(msg):
+	global pub
+	
+	msg.header.frame_id = "world"
+	msg.child_frame_id = "base_link"
+	pub.publish(msg)
+    
 
 if __name__ == "__main__":
-    rospy.init_node('odometry', anonymous=True) #make node 
-    rospy.Subscriber('/raas_drone/odom',Odometry,odometryCb)
+    rospy.init_node('odometry', anonymous=True) #make node
+    model = rospy.get_param("model")
+
+    rospy.Subscriber('/%s/odom' % model, Odometry, callback)
     rospy.spin()
